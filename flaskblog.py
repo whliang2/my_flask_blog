a52b0eae6cd188fc97c36ca2b599f16eaaef496f
whliang2 <whliang2@illinois.edu>
2020-09-13 00:32:35 -0500

    add login and register function pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisttrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0a0f7201aa53977e832eeab846cac2e3'
+
+
 
 posts =[
     {
@@ -28,6 +33,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisttrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data} !', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You Have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password!','danger')
+    return render_template('login.html', title='Login', form=form)
+
+
 
 
 
